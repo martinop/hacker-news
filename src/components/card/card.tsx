03e83ade392f5c0a949d87a03f37ffaf7a7e1a39
Martin Ocando <martinocandop@gmail.com>
2022-03-23 18:22:30 -0300

    type added

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ForwardedRef } from "react";
 import dayjs from "dayjs";
 
 import { NewArticle } from "../../types";
@@ -10,39 +10,41 @@ type CardProps = NewArticle & {
   isFavorite: boolean;
 };
 
-const Card = React.forwardRef((props: CardProps, ref: any) => {
-  const { title, author, storyUrl, createdAt, isFavorite } = props;
+const Card = React.forwardRef(
+  (props: CardProps, ref: ForwardedRef<HTMLDivElement>) => {
+    const { title, author, storyUrl, createdAt, isFavorite } = props;
 
-  function onOpen() {
-    window.open(storyUrl, "_blank");
-  }
+    function onOpen() {
+      window.open(storyUrl, "_blank");
+    }
 
-  return (
-    <div className="card" onClick={onOpen} ref={ref}>
-      <div className="card-content">
-        <div className="card-timeago">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="var(--color-gray-500)"
-              d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 12v-6h-2v8h7v-2h-5z"
-            />
-          </svg>
-          <span>
-            {dayjs(createdAt).fromNow()} by {author}
-          </span>
+    return (
+      <div className="card" onClick={onOpen} {...(ref !== null && { ref })}>
+        <div className="card-content">
+          <div className="card-timeago">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="var(--color-gray-500)"
+                d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 12v-6h-2v8h7v-2h-5z"
+              />
+            </svg>
+            <span>
+              {dayjs(createdAt).fromNow()} by {author}
+            </span>
+          </div>
+          <p className="card-title">{title}</p>
         </div>
-        <p className="card-title">{title}</p>
+        <div className="card-favorite">
+          {isFavorite ? <FilledHeart /> : <EmptyHeart />}
+        </div>
       </div>
-      <div className="card-favorite">
-        {isFavorite ? <FilledHeart /> : <EmptyHeart />}
-      </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Card;
