f3f4c86da48947cec6f2899512efa6d22ca12a29
Martin Ocando <martinocandop@gmail.com>
2022-03-22 21:15:06 -0300

    nav with style and test, also some colors were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Nav from "./components/nav";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
