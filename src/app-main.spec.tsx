d9f0f4620f4b014d4c5cd04e1ae0ef85d9bf402a
Martin Ocando <martinocandop@gmail.com>
2022-03-24 14:12:09 -0300

    integration testing

---
+++ src/app-main.spec.tsx
@@ -0,0 +1,33 @@
+import App from "./app-main";
+
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("<App />", () => {
+  it("should render", async () => {
+    render(<App />);
+    expect(screen.getByTestId("app")).toBeInTheDocument();
+  });
+
+  it("should change dropdown value", async () => {
+    render(<App />);
+
+    const dropdownValue = screen.getByText(/react/i); // initial value or could use dropdown-value test id
+
+    fireEvent.click(dropdownValue);
+
+    const item = screen.getAllByTestId("dropdown-item");
+    fireEvent.click(item[2]);
+
+    expect(screen.queryByText(/react/i)).not.toBeInTheDocument();
+  });
+
+  it("should change tab to my faves on click tab", async () => {
+    render(<App />);
+
+    const element = screen.getByText(/my faves/gi);
+    fireEvent.click(element);
+
+    const list = screen.getByTestId("favorites-list");
+    expect(list).toBeEmptyDOMElement();
+  });
+});
