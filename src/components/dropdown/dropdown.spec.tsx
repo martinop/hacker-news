6faa9273038f323146de13da9c305de7e0231120
Martin Ocando <martinocandop@gmail.com>
2022-03-24 11:29:15 -0300

    dropdown render and click tests

---
+++ src/components/dropdown/dropdown.spec.tsx
@@ -0,0 +1,36 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import Dropdown from "./dropdown";
+
+describe("<Dropdown />", () => {
+  it("should render placeholder", async () => {
+    render(<Dropdown value="" onChange={jest.fn} />);
+
+    const placeholder = screen.getByText(/select your news/i);
+    expect(placeholder).toBeInTheDocument();
+  });
+  it("should open and render list on click", async () => {
+    render(<Dropdown value="" onChange={jest.fn} />);
+
+    const placeholder = screen.getByText(/select your news/gi);
+
+    fireEvent.click(placeholder);
+
+    expect(screen.getByTestId("dropdown-list")).toBeInTheDocument();
+  });
+
+  it("should call onchange on click item", async () => {
+    const mockedOnClick = jest.fn();
+
+    render(<Dropdown value="" onChange={mockedOnClick} />);
+
+    const placeholder = screen.getByText(/select your news/gi);
+
+    fireEvent.click(placeholder);
+    expect(await screen.findByTestId("dropdown-list")).toBeInTheDocument();
+
+    const item = screen.getAllByTestId("dropdown-item");
+    fireEvent.click(item[0]);
+
+    expect(mockedOnClick).toHaveBeenCalledTimes(1);
+  });
+});
